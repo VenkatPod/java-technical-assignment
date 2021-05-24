9790a8639ecc22dd17ef74c7f9de4b2652eb8691
Venkat Podduturi <venkat.podduturi@sainsburys.co.uk>
2021-05-24 23:11:36 +0100

    completing the buyonekiloforhalf discount

--- src/main/java/kata/supermarket/discount/BuyOneKiloForHalfPriceDiscount.java
+++ src/main/java/kata/supermarket/discount/BuyOneKiloForHalfPriceDiscount.java
@@ -15,6 +15,13 @@ public class BuyOneKiloForHalfPriceDiscount extends Discount {
 
     @Override
     public BigDecimal applyDiscount(BigDecimal quantity, Product product) {
+        quantity.setScale(2, RoundingMode.FLOOR);
+        BigDecimal rounded = BigDecimal.valueOf(quantity.intValue());
+        if (quantity.intValue() >= 1) {
+            BigDecimal productPrice = ((WeighedProduct)product).weighing(BigDecimal.ONE).price();
+            return rounded.multiply(productPrice.divide(BigDecimal.valueOf(2)))
+                    .setScale(2, RoundingMode.HALF_UP);
+        }
         return BigDecimal.ZERO;
     }
 }
